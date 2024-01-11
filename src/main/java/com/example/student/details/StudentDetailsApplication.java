69e2576241d74e0b9023ead7e8a0e2f9bebb0bb5
iamramyak <iamramyak7@gmail.com>
2024-01-11 13:38:15 +0530

    student details

---
+++ src/main/java/com/example/student/details/StudentDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.example.student.details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDetailsApplication.class, args);
+	}
+
+}
